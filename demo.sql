2c4105feb4f9be1360235849cd6b71b37b37f58d
Nikhil Gupta <80402226+Nikhil1479@users.noreply.github.com>
2022-03-12 21:58:13 +0530

    Update and rename demo.sql to Assignment5.sql

--- demo.sql
+++
@@ -1,9 +0,0 @@
-SQL> select * from employee;
-
-EMPLOYEEID LNAME                FNAME                POSITIONID SUPERVISOR           HIREDATE      SALARY COMMISSION     DEPTID     QUALID
----------- -------------------- -------------------- ---------- -------------------- --------- ---------- ---------- ---------- ----------
-       111 Smith                John                          1                      15-APR-60     320650      35000         10          1
-       246 Houstan              Larry                         2 111                  19-MAY-67     150000      10000         40          2
-       123 Gupta                Nikhil                        2 111                  12-FEB-91      75000                    10          2
-       543 Dave                 Derek                         2 111                  15-MAR-95      80000      20000         20          1
-       433 McCall               Alex                          3 543                  05-MAY-97      66500                    20          4
